7ba74b22c0421d002eb2abebca8a88d7c7849e58
profilore <mamomi@yandex.ru>
2022-08-21 14:42:18 +0000

    made exit1 work

--- minishell_test/minishell.c
+++ minishell_test/minishell.c
@@ -1,13 +1,21 @@
 #include "minishell.h"
-
+#include "libft/libft.h"
 
 
 int	chck_exit(char *str)
 {
-	if (!strncmp("exit", str, 4))
+	if (!ft_strncmp("exit", str, 4))
 		if (!str[4])
 			return (0);
 	return (1);
+}
+
+int	chck_exit1(char *str)
+{
+	if (!ft_strncmp("exit 1", str, 6))
+		if (!str[6])
+			return(0);
+	return(1);
 }
 
 int	main()
@@ -17,12 +25,17 @@ int	main()
 	line = readline(BEGIN(49, 34)"Myshell $> "CLOSE);
 	if (!line)
 		exit(0);
-	while (chck_exit(line))
+	while (chck_exit(line) && chck_exit1(line))
 	{
 		add_history(line);
 		printf("%s\n", line);
 		free(line);
 		line = readline(BEGIN(49, 34)"Myshell $> "CLOSE);
+	}
+	if (!chck_exit1(line))
+	{
+		free(line);
+		exit(1);
 	}
 	free(line);
 	return (0);
