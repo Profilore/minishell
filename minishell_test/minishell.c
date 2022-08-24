9b9b4862ca74a33c46bb761d793007f94703f2d4
profilore <mamomi@yandex.ru>
2022-08-24 13:50:08 +0000

    splitting and printing

--- minishell_test/minishell.c
+++ minishell_test/minishell.c
@@ -1,41 +1,215 @@
 #include "minishell.h"
 #include "libft/libft.h"
 
+t_exec	*exec;
 
-int	chck_exit(char *str)
+void	exit_on_err(char *msg)
 {
-	if (!ft_strncmp("exit", str, 4))
-		if (!str[4])
-			return (0);
-	return (1);
+	ft_putstr_fd(msg, 2);
+	free_all();
+	exit(1);
 }
 
-int	chck_exit1(char *str)
+void	free_lst(void)
 {
-	if (!ft_strncmp("exit 1", str, 6))
-		if (!str[6])
-			return(0);
-	return(1);
+	t_lst	*tmp;
+
+	while (exec->lst->next != NULL)
+	{
+		tmp = exec->lst->next;
+		free(exec->lst->str);
+		free(exec->lst);
+		exec->lst = tmp;
+	}
+	free(exec->lst->str);
+	free(exec->lst);
+	exec->lst = NULL;
 }
+
+void	free_all(void)
+{
+	t_data	*tmp;
+
+	while (exec->data->next != NULL)
+	{
+		tmp = exec->data->next;
+		free(exec->data->cmd);
+		free(exec->data);
+		exec->data = tmp;
+	}
+	free_lst();
+	free(exec->data->cmd);
+	free(exec->data);
+	free(exec->line);
+	free(exec);
+}
+
+t_cmd	*init_str_cmd(void)
+{
+	t_cmd	*cmd;
+
+	cmd = (t_cmd *)malloc(sizeof(t_cmd));
+	if (!cmd)
+		exit_on_err("failed malloc");
+	cmd->name = NULL;
+	cmd->l_pipe[0] = 0;
+	cmd->l_pipe[1] = 0;
+	cmd->r_pipe[0] = 0;
+	cmd->r_pipe[1] = 0;
+	cmd->imt_file = NULL;
+	cmd->otp_file = NULL;
+	cmd->arg_type = INT;
+	cmd->arg.nb = 0;
+	cmd->opt = 0;
+	cmd->f = NULL;
+	return (cmd);
+}
+
+t_data	*init_str_data(void)
+{
+	t_data	*data;
+
+	data = (t_data *)malloc(sizeof(t_data));
+	if (!data)
+		exit_on_err("failed malloc");
+	data->cmd = init_str_cmd();
+	data->next = NULL;
+	return (data);
+}
+
+void	init_struct(void)
+{
+	exec = (t_exec *)malloc(sizeof(t_exec));
+	if (!exec)
+		exit_on_err("failed malloc");
+	exec->exit = 0;
+	exec->line = NULL;
+	exec->lst = NULL;
+	exec->data = init_str_data();
+}
+
+t_lst	*init_list(void)
+{
+	t_lst	*lst;
+
+	lst = (t_lst *)malloc(sizeof(t_lst));
+	if (!lst)
+		exit_on_err("failed listing");
+	lst->str = NULL;
+	lst->tok = 0;
+	lst->next = NULL;
+	return (lst);
+}
+
+t_lst	*add_lst_node(t_lst *prev)
+{
+	t_lst	*new;
+
+	new = init_list();
+	prev->next = new;
+	return (new);
+}
+
+void	make_lst(char **wrd_list)
+{
+	char	*content;
+	int		len;
+	int		i;
+	t_lst	*tmp;
+
+	i = 0;
+	exec->lst = init_list();
+	tmp = exec->lst;
+	while (wrd_list[i])
+	{
+		printf("word %i: %s\n", i, wrd_list[i]);
+		i++;
+	}
+	i = 0;
+	while (wrd_list[i])
+	{
+		len = ft_strlen(wrd_list[i]) + 1;
+		content = malloc(len);
+		if (!content)
+		{
+			free_lst();
+			exit_on_err("failed malloc");
+		}
+		ft_strlcpy(content, wrd_list[i], len);
+		printf("content: %s\n", content);
+		tmp->str = content;
+		printf("%s\n", tmp->str);
+		i++;
+		tmp->next = add_lst_node(tmp);// one list node too many lol
+		tmp = tmp->next;
+	}
+	i = 0;
+	while (wrd_list[i])
+	{
+		free(wrd_list[i]);
+		i++;
+	}
+	free(wrd_list);
+}
+
+void	lexer_expander(char *line)
+{
+	char	**wrd_list;
+
+	wrd_list = ft_split(line, ' ');
+	if (!wrd_list)
+		exit_on_err("failed split");
+	make_lst(wrd_list);
+//	check_quotes();
+//	check_redrir_pipes();
+//	check_env_vars();
+}
+
+void	fill_struct(char *line)
+{
+	t_lst	*tmp;
+
+	tmp = exec->lst;
+	lexer_expander(line);
+	if (!ft_strncmp(exec->lst->str, "exit", 4))
+	{
+		free_all();
+		exit(0);
+	}
+	while (exec->lst->next != NULL)
+	{
+		printf("%s, ", exec->lst->str);
+		exec->lst = exec->lst->next;
+	}
+	printf("%s \n", exec->lst->str);
+}
+/*
+void		do_stuff(t_exec *exec)
+{
+	if (exec->exit != 0)
+	{
+		free_all(exec);
+		return ;
+	}
+}*/
 
 int	main()
 {
 	char	*line;
 
+	init_struct();
 	line = readline(BEGIN(49, 34)"Myshell $> "CLOSE);
 	if (!line)
 		exit(0);
-	while (chck_exit(line) && chck_exit1(line))
+	while (1)
 	{
 		add_history(line);
-		printf("%s\n", line);
+		fill_struct(line);
+//		do_stuff(exec);
+//		if (exec->exit != 0)
+//			break ;
 		free(line);
 		line = readline(BEGIN(49, 34)"Myshell $> "CLOSE);
-	}
-	if (!chck_exit1(line))
-	{
-		free(line);
-		exit(1);
 	}
 	free(line);
 	return (0);
--- minishell_test/minishell.h
+++ minishell_test/minishell.h
@@ -6,8 +6,90 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+
+typedef union u_arg
+{
+	int			nb;
+	long long	ln;
+	char		ch;
+	char		*str;
+	void		*ptr;
+} t_arg;
+
+enum e_type
+{
+	INT,
+	LONG_LONG,
+	CHAR,
+	STRING,
+	PTR
+};
+
+typedef struct s_lst
+{
+	char			*str;
+	int				tok;
+	struct s_lst	*next;
+} t_lst;
+
+typedef struct s_cmd
+{
+	char		*name;
+	int			l_pipe[2];
+	int 		r_pipe[2];
+	char		*imt_file;
+	char		*otp_file;
+	enum e_type	arg_type;
+	t_arg		arg;
+	char		opt;
+	void		*f;
+} t_cmd;
+
+enum e_blt_in
+{
+	ECHO,
+	CD,
+	PWD,
+	EXPORT,
+	UNSET,
+	ENV,
+	EXIT,
+	NO
+};
+
+typedef struct s_func
+{
+	enum e_blt_in	b_cmd;
+	void			*f;
+} t_func;
+
+typedef struct s_data
+{
+	t_cmd			*cmd;
+	struct s_data	*next;
+} t_data;
+
+typedef struct s_exec
+{
+	char			*line;
+	int				exit;
+	t_lst			*lst;
+	t_data			*data;
+} t_exec;
+
 #define CLOSE "\001\033[0m\002"                 // Закрыть все свойства
 #define BLOD  "\001\033[1m\002"                 // Подчеркнуть, жирным шрифтом, выделить
 #define BEGIN(x,y) "\001\033["#x";"#y"m\002"    // x: background, y: foreground
+
+void	exit_on_err(char *msg);
+void	free_lst(void);
+void	free_all(void);
+void	init_struct(void);
+t_lst	*init_list(void);
+t_lst	*add_lst_node(t_lst *prev);
+void	make_lst(char **wrd_list);
+void	lexer_expander(char *line);
+void	fill_struct(char *line);
+
 
 #endif
